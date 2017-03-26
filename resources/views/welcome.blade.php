4ce7fcb03d9c0fe5b4eb3b1f32ee5e3e85c94a3e
kangzengyong <kzrvip@gmail.com>
2017-03-26 15:24:20 +0800

    webhook test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 测试
                 </div>
 
                 <div class="links">
